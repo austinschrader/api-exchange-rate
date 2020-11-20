bc5057a0e250d9099d87c274b0a8308a52312c98
Austin Schrader <austin.jschrader@gmail.com>
2020-11-20 13:44:17 -0800

    Add async makeApiCall & getElements

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './js/CurrencyService.js';
 
+async function makeApiCall() {
+  const response = await NasaService.getConversion();
+  getElements(response);
+}
+
+function getElements(response) {
+  console.log(response);
+}
+
 $(document).ready(function () {
   $('#formOne').submit(function (event) {
     event.preventDefault();
