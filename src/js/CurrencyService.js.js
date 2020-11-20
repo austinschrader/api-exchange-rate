bd60a93d75e0cf5ee6db7f12bd206e9469b566ff
Austin Schrader <austin.jschrader@gmail.com>
2020-11-20 13:00:59 -0800

    Add in API call to exchangerate-api.com

---
+++ src/js/CurrencyService.js.js
@@ -0,0 +1,9 @@
+export default class CurrencyService {
+  static async getConversion() {
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+      );
+    }
+  }
+}
