8937c4c7fd44b83fe78adfaaafb9712720e69c20
Austin Schrader <austin.jschrader@gmail.com>
2020-11-20 13:41:00 -0800

    Update import statement to import CurrencyService

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import { User } from './js/user.js';
+import CurrencyService from './js/CurrencyService.js';
 
 $(document).ready(function () {
   $('#formOne').submit(function (event) {
