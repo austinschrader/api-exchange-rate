3e3bc51c2526acdab736343d3f7a51f0c28a85b6
Austin Schrader <austin.jschrader@gmail.com>
2020-11-20 15:03:28 -0800

    Add in error handling for the promise

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -4,6 +4,12 @@ export default class CurrencyService {
       const response = await fetch(
         `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currencyFrom}`
       );
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
     }
   }
 }
