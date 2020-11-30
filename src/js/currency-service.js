c48b0f2c66dbb657c087a99e4a3888f1c9e302a7
Austin Schrader <austin.jschrader@gmail.com>
2020-11-29 23:02:20 -0800

    Add variable simplification

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -2,7 +2,7 @@ export default class CurrencyService {
   static async getConversion() {
     try {
       const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currencyFrom}`
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
       );
       if (!response.ok) {
         throw Error(response.statusText);
